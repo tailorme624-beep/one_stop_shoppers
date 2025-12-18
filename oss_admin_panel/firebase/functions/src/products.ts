8679124fddf861199c97ad8d55ded0280fda9968
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 19:43:36 +0300

    Update products.ts

--- oss_admin_panel/firebase/functions/src/products.ts
+++ oss_admin_panel/firebase/functions/src/products.ts
@@ -1,1 +1,10 @@
+export const lockProductApproval = functions.firestore
+  .document("products/{productId}")
+  .onCreate(async (snap, context) => {
+    const product = snap.data();
+
+    if (product.status !== "active") {
+      await snap.ref.update({ status: "pending" });
+    }
+  });
 
