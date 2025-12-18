7507d1d61ea2c56d85d9678dacf09827f30eaa56
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 19:32:37 +0300

    Update admin_routes.dart

--- oss_admin_panel/lib/routes/admin_routes.dart
+++ oss_admin_panel/lib/routes/admin_routes.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import '../features/sellers/seller_approval_screen.dart';
 import '../features/products/product_queue_screen.dart';
+import '../features/orders/order_oversight_screen.dart';
 
 class AdminRoutes {
   static const sellers = '/sellers';
   static const products = '/products';
+static const orders = '/orders';
 
+routes[orders] = (_) => const OrderOversightScreen();
   static Map<String, WidgetBuilder> routes = {
     sellers: (_) => const SellerApprovalScreen(),
     products: (_) => const ProductQueueScreen(),
   };
+  
 }
 
