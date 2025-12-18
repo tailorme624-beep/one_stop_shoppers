3c7f0a719a3b22b86743db4a0064dfb64e4e455b
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 14:23:16 +0300

    Update product_detail_screen.dart

--- lib/features/buyer/product/product_detail_screen.dart
+++ lib/features/buyer/product/product_detail_screen.dart
@@ -1,1 +1,73 @@
+import 'package:flutter/material.dart';
+import '../models/product_model.dart';
+import '../cart/cart_controller.dart';
+import '../../../core/widgets/primary_button.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  final Product product;
+
+  const ProductDetailScreen({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(product.name)),
+      body: Column(
+        children: [
+          Container(
+            height: 250,
+            color: Colors.grey[300],
+            child: Center(
+              child: Text('Product Image'),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+
+                  Text(
+                    'UGX ${product.price.toStringAsFixed(0)}',
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.green,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+
+                  Text(product.description),
+                  const Spacer(),
+
+                  PrimaryButton(
+                    title: 'Add to Cart',
+                    onPressed: () {
+                      CartController.addToCart(product);
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Added to cart'),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
