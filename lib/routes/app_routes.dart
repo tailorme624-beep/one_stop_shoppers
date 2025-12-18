914ff31b99d74bb9332bc8cd7ab1bcd70310e90c
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 14:41:13 +0300

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,17 +1,16 @@
-import 'package:flutter/material.dart';
-import '../features/buyer/auth/login_screen.dart';
-import '../features/buyer/home/home_screen.dart';
-import '../features/buyer/cart/cart_screen.dart';
+import '../features/buyer/checkout/shipping_address_screen.dart';
+import '../features/buyer/checkout/payment_method_screen.dart';
+import '../features/buyer/checkout/order_review_screen.dart';
 
-class AppRoutes {
-  static const login = '/';
-  static const home = '/home';
-  static const cart = '/cart';
+static const shipping = '/shipping';
+static const payment = '/payment';
+static const review = '/review';
 
-  static Map<String, WidgetBuilder> routes = {
-    login: (_) => const LoginScreen(),
-    home: (_) => const HomeScreen(),
-    cart: (_) => const CartScreen(),
-  };
-}
-
+static Map<String, WidgetBuilder> routes = {
+  login: (_) => const LoginScreen(),
+  home: (_) => const HomeScreen(),
+  cart: (_) => const CartScreen(),
+  shipping: (_) => const ShippingAddressScreen(),
+  payment: (_) => const PaymentMethodScreen(),
+  review: (_) => const OrderReviewScreen(),
+};
