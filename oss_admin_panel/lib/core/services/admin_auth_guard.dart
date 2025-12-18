b81b9aa225264fa7e1b8c1af9201097b91a8caba
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 18:35:20 +0300

    Update admin_auth_guard.dart

--- oss_admin_panel/lib/core/services/admin_auth_guard.dart
+++ oss_admin_panel/lib/core/services/admin_auth_guard.dart
@@ -1,1 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AdminAuthGuard {
+  static Future<bool> isAdmin() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return false;
+
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    return doc.exists && doc['role'] == 'admin';
+  }
+}
 
