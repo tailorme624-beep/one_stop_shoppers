3b9bb3968d8136272c27145edebe531af9abc827
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 13:29:26 +0300

    Update app_colors.dart

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF307345);
+  static const background = Color(0xFFF5F1EA);
+  static const textDark = Color(0xFF1E1E1E);
+  static const textLight = Color(0xFF6B6B6B);
+}
 
