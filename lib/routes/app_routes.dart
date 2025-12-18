4bb02cad69d8e2a7a4f0ab7396a91b5f6e0f3e61
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 13:35:34 +0300

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import '../features/buyer/auth/login_screen.dart';
+import '../features/buyer/home/home_screen.dart';
+import '../features/buyer/cart/cart_screen.dart';
+
+class AppRoutes {
+  static const login = '/';
+  static const home = '/home';
+  static const cart = '/cart';
+
+  static Map<String, WidgetBuilder> routes = {
+    login: (_) => const LoginScreen(),
+    home: (_) => const HomeScreen(),
+    cart: (_) => const CartScreen(),
+  };
+}
 
