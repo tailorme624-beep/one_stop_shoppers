16b2f437c1bcc518bf9ab754da6729c89312755f
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 15:51:26 +0300

    Create order_service.dart

---
+++ lib/features/buyer/order/order_service.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'order_model.dart';
+import '../cart/cart_controller.dart';
+
+class OrderService {
+  final _db = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+
+  Future<void> placeOrder(String paymentMethod) async {
+    final user = _auth.currentUser!;
+    final items = CartController.items;
+
+    final orderItems = items.map((product) {
+      return {
+        'productId': product.id,
+        'name': product.name,
+        'price': product.price,
+        'quantity': 1,
+      };
+    }).toList();
+
+    await _db.collection('orders').add({
+      'buyerId': user.uid,
+      'items': orderItems,
+      'totalAmount': CartController.totalPrice,
+      'paymentMethod': paymentMethod,
+      'orderStatus': 'processing',
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+
+    CartController.items.clear();
+  }
+
+  Stream<QuerySnapshot> getBuyerOrders() {
+    final user = _auth.currentUser!;
+    return _db
+        .collection('orders')
+        .where('buyerId', isEqualTo: user.uid)
+        .orderBy('createdAt', descending: true)
+        .snapshots();
+  }
+}
