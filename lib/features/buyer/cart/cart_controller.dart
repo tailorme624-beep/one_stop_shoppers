ecbe71587d5724161c2f9ecc56389d6672e2ad30
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 14:21:18 +0300

    Update cart_controller.dart

--- lib/features/buyer/cart/cart_controller.dart
+++ lib/features/buyer/cart/cart_controller.dart
@@ -1,1 +1,19 @@
+import '../models/product_model.dart';
 
+class CartController {
+  static final List<Product> _cartItems = [];
+
+  static List<Product> get items => _cartItems;
+
+  static void addToCart(Product product) {
+    _cartItems.add(product);
+  }
+
+  static void removeFromCart(Product product) {
+    _cartItems.remove(product);
+  }
+
+  static double get totalPrice {
+    return _cartItems.fold(0, (sum, item) => sum + item.price);
+  }
+}
