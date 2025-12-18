76e542c3d17e0b6fafaa537053ebf4f8071695d8
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 15:10:10 +0300

    Update login_screen.dart

--- lib/features/buyer/auth/login_screen.dart
+++ lib/features/buyer/auth/login_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import '../../../core/widgets/primary_button.dart';
+import '../../../core/services/auth_service.dart';
+
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final auth = AuthService();
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
@@ -28,13 +31,14 @@ class LoginScreen extends StatelessWidget {
               decoration: InputDecoration(labelText: 'Password'),
             ),
             const SizedBox(height: 30),
-
-            PrimaryButton(
-              title: 'Login',
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-            ),
+          
+                    PrimaryButton(
+            title: 'Login',
+            onPressed: () async {
+              await auth.login(email, password);
+              Navigator.pushReplacementNamed(context, '/home');
+            },
+          ),
           ],
         ),
       ),
