12ceebb43f00fe8c0c29abf912420e1ccabeefbd
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 13:33:02 +0300

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'core/theme/app_theme.dart';
+import 'routes/app_routes.dart';
+
+class OSSApp extends StatelessWidget {
+  const OSSApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'One Stop Shoppers',
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.login,
+      routes: AppRoutes.routes,
+    );
+  }
+}
 
