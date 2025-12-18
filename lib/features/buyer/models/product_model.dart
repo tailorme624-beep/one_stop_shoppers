a69eb08f90d65ccb6da0b2f2bbc1d42c7df8f7e7
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 15:20:58 +0300

    Update product_model.dart

--- lib/features/buyer/models/product_model.dart
+++ lib/features/buyer/models/product_model.dart
@@ -3,14 +3,23 @@ class Product {
   final String name;
   final double price;
   final String description;
-  final String image;
+  final List<String> images;
 
   Product({
     required this.id,
     required this.name,
     required this.price,
     required this.description,
-    required this.image,
+    required this.images,
   });
-}
 
+  factory Product.fromFirestore(String id, Map<String, dynamic> data) {
+    return Product(
+      id: id,
+      name: data['name'],
+      price: (data['price'] as num).toDouble(),
+      description: data['description'],
+      images: List<String>.from(data['images'] ?? []),
+    );
+  }
+}
