4154a7c52658b7532d40e107c36a63748c1a9c9c
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 15:06:48 +0300

    Update auth_service.dart

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,1 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthService {
+  final _auth = FirebaseAuth.instance;
+  final _db = FirebaseFirestore.instance;
+
+  Future<void> registerBuyer(String email, String password) async {
+    final userCred = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    await _db.collection('users').doc(userCred.user!.uid).set({
+      'uid': userCred.user!.uid,
+      'email': email,
+      'role': 'buyer',
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+  }
+
+  Future<void> login(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Stream<User?> authState() => _auth.authStateChanges();
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
 
