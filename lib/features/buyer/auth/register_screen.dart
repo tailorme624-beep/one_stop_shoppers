fe2c30f1656c641991247c7ab7694a2a98fdbe51
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 15:07:37 +0300

    Update register_screen.dart

--- lib/features/buyer/auth/register_screen.dart
+++ lib/features/buyer/auth/register_screen.dart
@@ -1,1 +1,48 @@
+import 'package:flutter/material.dart';
+import '../../../core/services/auth_service.dart';
+
+class RegisterScreen extends StatelessWidget {
+  RegisterScreen({super.key});
+
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+  final auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Create Account')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailCtrl,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(height: 10),
+
+            TextField(
+              controller: passCtrl,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () async {
+                await auth.registerBuyer(
+                  emailCtrl.text,
+                  passCtrl.text,
+                );
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: const Text('Register'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
