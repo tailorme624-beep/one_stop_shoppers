7d1d7a0ccecbb9de3e657ed21e3a0c59a7eeee6f
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 19:45:04 +0300

    Update index.ts

--- oss_admin_panel/firebase/functions/src/index.ts
+++ oss_admin_panel/firebase/functions/src/index.ts
@@ -1,1 +1,8 @@
+import * as admin from "firebase-admin";
+admin.initializeApp();
+
+export * from "./notifications";
+export * from "./orders";
+export * from "./products";
+export * from "./stats";
 
