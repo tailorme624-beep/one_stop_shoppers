4c4d21ab832746dcbbd6218b030e8065c4991bfb
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 14:19:41 +0300

    Update product_model.dart

--- lib/features/buyer/models/product_model.dart
+++ lib/features/buyer/models/product_model.dart
@@ -1,1 +1,16 @@
+class Product {
+  final String id;
+  final String name;
+  final double price;
+  final String description;
+  final String image;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.image,
+  });
+}
 
