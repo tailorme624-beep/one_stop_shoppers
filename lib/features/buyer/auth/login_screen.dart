810b93caa6139d4ee613058981d4201f69b77a37
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 13:33:54 +0300

    Update login_screen.dart

--- lib/features/buyer/auth/login_screen.dart
+++ lib/features/buyer/auth/login_screen.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+import '../../../core/widgets/primary_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to One Stop Shoppers',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(height: 15),
+
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height: 30),
+
+            PrimaryButton(
+              title: 'Login',
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
