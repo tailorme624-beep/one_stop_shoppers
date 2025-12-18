e78f6cdb428d26d9574f8511dc240c196606451e
tailorme624-beep <tailorme624@gmail.com>
2025-12-18 13:30:15 +0300

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.background,
+    primaryColor: AppColors.primary,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      foregroundColor: AppColors.textDark,
+      elevation: 0,
+    ),
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      bodyMedium: TextStyle(fontSize: 14),
+    ),
+  );
+}
 
